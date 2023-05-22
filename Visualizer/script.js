25d2ba32030a31c0a0a80ee6ff54dd4981441b45
Max Linke <maxlinke@users.noreply.github.com>
2023-05-22 17:42:35 +0200

    added testdata to save time, added agent ranking table

--- Visualizer/script.js
+++ Visualizer/script.js
@@ -5,20 +5,37 @@ document.addEventListener("DOMContentLoaded", () => {
     const winsId = "Wins";
     const lossesId = "Losses";
     const drawsId = "Draws";
-
     const displayOptions = [ winsId, lossesId, drawsId ];
     const displayOptionDropdown = document.getElementById("displayOptionsSelection");
     function getCurrentDisplayOption () { return displayOptionDropdown.value; }
 
-    const wlRatioMetric = "(Wins-Losses)/Games";
+    const winPercentMetric = "Win%";
+    const wlRatioMetric = "W/L-Ratio";
     const eloMetric = "Elo (TODO)";
-    const performanceMetrics = [ wlRatioMetric, eloMetric ];
-    // TODO dropdown for that too
-    // so don't sort the participant ids in the processing step
-    // but rather just assign the scores there
-    // and the sorting can happen in here
-    // show the raw data in a different table (i'll have to refactor my code in here)
-    // agentid wins losses draws w/l elo
+    const rankingOptions = [ winPercentMetric, wlRatioMetric, eloMetric ];
+    const rankingOptionsDropdown = document.getElementById("rankingOptionsSelection");
+    function getCurrentRankingOption () { return rankingOptionsDropdown.value; }
+
+    const rankingTable = document.getElementById("agentRankingTable");
+    const matchupMatrix = document.getElementById("matchupMatrix");
+
+    const rankingColumnLabels = [ "", "Wins", "Wins%", "Losses", "Losses%", "Draws", "Draws%", "W/L-Ratio", "Elo" ];
+    function getRankingColumnData (playerData, column) {
+        switch(column){
+            case 0: return playerData.shortId;
+            case 1: return playerData.totalWins;
+            case 2: return `${(100 * playerData.winPercentage).toFixed(2)}`;
+            case 3: return playerData.totalLosses;
+            case 4: return `${(100 * playerData.lossPercentage).toFixed(2)}`;
+            case 5: return playerData.totalDraws;
+            case 6: return `${(100 * playerData.drawPercentage).toFixed(2)}`;
+            case 7: return playerData.winLossRatio.toFixed(5);
+            case 8: return playerData.elo.toFixed(1);
+        }
+    }
+
+    let loadedData = undefined;
+    let sortedPlayerIndices = [];
 
     async function handleFileSelect(evt) {
         const reader = new FileReader();
@@ -32,62 +49,109 @@ document.addEventListener("DOMContentLoaded", () => {
                 // TODO display this somewhere
                 console.error(error);
                 loadedData = undefined;
-                clearTable();
             }
         }
         reader.readAsText(evt.target.files[0]);
     }
 
-    let loadedData = undefined;
-
     function onDataLoaded (input) {
-        clearTable();
         loadedData = processData(input);
         console.log(loadedData);
-        updateTable();
+        updateRankingTable();
+        updateMatrix();
         // TODO add the option to select participants if more than 2 players per matchup (include "any" option to average all together)
     }
 
     function onDisplayOptionChanged () {
-        console.log(getCurrentDisplayOption());
         if(loadedData != undefined){
-            updateTable();
+            updateMatrix();
         }
     }
 
-    function clearTable () {
-        const tables = document.getElementsByTagName("table");
-        for(let i=0; i<tables.length; i++){
-            const table = tables[i];
-            table.parentElement.removeChild(table);
+    function onRankingOptionChanged () {
+        if(loadedData != undefined){
+            updateRankingTable();
+            updateMatrix();
         }
     }
 
-    function getSimpleWinLossDrawRatio (wins, losses, draws) {
-        const output = [];
-        for(let i=0; i<wins.length; i++){
-            output[i] = (wins[i] - losses[i]) / (wins[i] + losses[i] + draws[i]);
-        }
-        return output;
+    function updateSortedPlayerIndices () {
+        const tempMap = loadedData.players.map((v, i) => { return v; });
+        const currentRankingOption = getCurrentRankingOption();
+        console.log
+        tempMap.sort((a, b) => {
+            let aVal = NaN;
+            let bVal = NaN;
+            switch(currentRankingOption){
+                case winPercentMetric: aVal = a.winPercentage; bVal = b.winPercentage; break;
+                case wlRatioMetric:    aVal = a.winLossRatio;  bVal = b.winLossRatio;  break;
+                case eloMetric:        aVal = a.elo;           bVal = b.elo;           break;
+            }
+            return (aVal > bVal ? 1 : (aVal < bVal) ? -1 : 0);
+        });
+        sortedPlayerIndices = tempMap.map((v, i) => v.index);
     }
 
-    function updateTable () {
-        clearTable();
-        // TODO data will be processed in above step
-        // document.getElementsByTagName("body")[0].appendChild(...
+    function updateRankingTable () {
+        rankingTable.replaceChildren();
+        if(loadedData.players.length > 0){
+            updateSortedPlayerIndices();
+            const firstRow = document.createElement("tr");
+            rankingColumnLabels.forEach(colLabel => {
+                const newLabel = document.createElement("th");
+                newLabel.className = "rankingTableColumnLabel";
+                newLabel.innerHTML = colLabel;
+                firstRow.appendChild(newLabel);
+            });
+            rankingTable.appendChild(firstRow);
+            sortedPlayerIndices.forEach(playerIndex => {
+                const playerData = loadedData.players[playerIndex];
+                const newRow = document.createElement("tr");
+                for(let i=0; i<rankingColumnLabels.length; i++){
+                    let newData;
+                    if(i == 0){
+                        newData = document.createElement("th");
+                        newData.className = "rankingTableRowLabel";
+                    }else{
+                        newData = document.createElement("td");
+                        newData.className = "rankingTableDataField";
+                    }
+                    newData.innerHTML = getRankingColumnData(playerData, i);
+                    newRow.appendChild(newData);
+                }
+                rankingTable.appendChild(newRow);
+            });
+        }
+        // TODO
+    }
+
+    function updateMatrix () {
+        matchupMatrix.replaceChildren();
+        // TODO
     }
 
 //  ----- init -----
 
+    function initDropdown (dropdown, options, onValueChanged) {
+        options.forEach((option) => {
+            const newOption = document.createElement("option");
+            dropdown.appendChild(newOption);
+            newOption.value = option;
+            newOption.innerHTML = option;
+        });
+        dropdown.value = options[0];
+        dropdown.addEventListener("change", () => onValueChanged());
+    }
+
     document.getElementById("fileInput").addEventListener("change", handleFileSelect, false);
-    displayOptions.forEach((option) => {
-        const newOption = document.createElement("option");
-        displayOptionDropdown.appendChild(newOption);
-        newOption.value = option;
-        newOption.innerHTML = option;
-    });
-    displayOptionDropdown.value = displayOptions[0];
-    displayOptionDropdown.addEventListener("change", () => onDisplayOptionChanged());
+    initDropdown(displayOptionDropdown, displayOptions, onDisplayOptionChanged);
+    initDropdown(rankingOptionsDropdown, rankingOptions, onRankingOptionChanged);
     // clearTable();
+    try{
+        onDataLoaded(testData);
+    }catch(e){
+        if(e instanceof ReferenceError) console.log("no test data");
+        else throw e;
+    }
 
 });
--- Visualizer/tournamentResultProcessor.js
+++ Visualizer/tournamentResultProcessor.js
@@ -3,27 +3,59 @@
 function processData (input) {
     // TODO unpack the compact data
     const output = {};
+    const playerGameCounts = input.playerIds.map((id, i) => {
+        return input.totalWins[i] + input.totalLosses[i] + input.totalDraws[i];
+    });
+    let shortestIdLength = Infinity;
+    let shortestId = "";
+    input.playerIds.forEach(id => {
+        if(id.length < shortestIdLength){
+            shortestIdLength = id.length;
+            shortestId = id;
+        }
+    });
+    let commonPrefix = "";
+    if(shortestId.includes(".")){
+        commonPrefix = shortestId.substring(0, shortestId.lastIndexOf(".") + 1);
+        input.playerIds.forEach(id => {
+            while(commonPrefix.length > 0){
+                if(id.startsWith(commonPrefix)){
+                    return;
+                }
+                commonPrefix = commonPrefix.substring(0, commonPrefix.length - 1);
+                commonPrefix = commonPrefix.substring(0, commonPrefix.lastIndexOf(".") + 1);
+            }
+        });
+    }
     output.players = input.playerIds.map((id, i) => { return { 
         id: id,
-        origIndex: i,
+        shortId: id.substring(commonPrefix.length),
+        index: i,
         totalWins: input.totalWins[i],
         totalLosses: input.totalLosses[i],
         totalDraws: input.totalDraws[i],
-        totalGamesPlayed: input.totalWins[i] + input.totalLosses[i] + input.totalDraws[i],
-        performance: (input.totalWins[i] - input.totalLosses[i]) / (input.totalWins[i] + input.totalLosses[i] + input.totalDraws[i])
+        totalGamesPlayed: playerGameCounts[i],
+        winPercentage: input.totalWins[i] / playerGameCounts[i],
+        lossPercentage: input.totalLosses[i] / playerGameCounts[i],
+        drawPercentage: input.totalDraws[i] / playerGameCounts[i],
+        winLossRatio: (input.totalWins[i] - input.totalLosses[i]) / (playerGameCounts[i]),
+        elo: NaN    // TODO
     }});
-    output.players.sort((a, b) => {
-        if(a.performance > b.performance) return 1;
-        if(a.performance < b.performance) return -1;
-        return 0;
-    });
+    // output.players.sort((a, b) => {
+    //     if(a.performance > b.performance) return 1;
+    //     if(a.performance < b.performance) return -1;
+    //     return 0;
+    // });
     console.log(output.players);
     // what now?
     // i need to unpack the matchup winners things
     // i guess i can just replace all the indices with the proper ids (and undefined for draws)
     // and replace the matchup indices with matchup objects
     output.matchups = input.matchupWinners.map((v, i) => {
-        return { };
+        // return {
+        //     // TODO
+        // };
+        return v;
     });
-    return input;
+    return output;
 }
