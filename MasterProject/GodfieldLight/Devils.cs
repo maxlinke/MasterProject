7d92d9b1ae925e52135c0d9164c83edc97e8e097
Max Linke <maxlinke@users.noreply.github.com>
2023-07-11 20:31:02 +0200

    tested and fixed devils code

--- MasterProject/GodfieldLight/Devils.cs
+++ MasterProject/GodfieldLight/Devils.cs
@@ -8,39 +8,39 @@ namespace MasterProject.GodfieldLight {
 
     public static class Devils {
 
-        const float DEVIL_CHANCE = 0.25f;
-        const int SMALL_DEVIL_OCC = 7;
-        const int SMALL_DEVIL_DMG = 10;
-        const int MEDIUM_DEVIL_OCC = 5;
-        const int MEDIUM_DEVIL_DMG = 20;
-        const int LARGE_DEVIL_OCC = 3;
-        const int LARGE_DEVIL_DMG = 30;
-        const int FAIRY_OCC = 5;
-        const int FAIRY_HEAL = 10;
+        public const float DEVIL_CHANCE = 0.25f;
+        public const int SMALL_DEVIL_OCC = 7;
+        public const int SMALL_DEVIL_DMG = 10;
+        public const int MEDIUM_DEVIL_OCC = 5;
+        public const int MEDIUM_DEVIL_DMG = 20;
+        public const int LARGE_DEVIL_OCC = 3;
+        public const int LARGE_DEVIL_DMG = 30;
+        public const int FAIRY_OCC = 5;
+        public const int FAIRY_HEAL = 10;
 
         public static bool CheckIfDevilAppears (System.Random rng, out string id, out int damage) {
             if (rng.NextDouble() < DEVIL_CHANCE) {
                 var i = rng.Next(SMALL_DEVIL_OCC + MEDIUM_DEVIL_OCC + LARGE_DEVIL_OCC + FAIRY_OCC);
                 i -= SMALL_DEVIL_OCC;
-                if (i <= 0) {
+                if (i < 0) {
                     id = "SmallDevil";
                     damage = SMALL_DEVIL_DMG;
                     return true;
                 }
                 i -= MEDIUM_DEVIL_OCC;
-                if (i <= 0) {
+                if (i < 0) {
                     id = "MediumDevil";
                     damage = MEDIUM_DEVIL_DMG;
                     return true;
                 }
                 i -= LARGE_DEVIL_OCC;
-                if (i <= 0) {
+                if (i < 0) {
                     id = "LargeDevil";
                     damage = LARGE_DEVIL_DMG;
                     return true;
                 }
                 i -= FAIRY_OCC;
-                if (i <= 0) {
+                if (i < 0) {
                     id = "CharityFairy";
                     damage = -FAIRY_HEAL;
                     return true;
---
+++ MasterProject/GodfieldLight/Tests/DevilsTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MasterProject.GodfieldLight.Tests {
+
+    [TestFixture]
+    public class DevilsTests {
+
+        [Test]
+        public void TestRatiosApproximately () {
+            var rng = new System.Random();
+            var smallCounter = 0;
+            var mediumCounter = 0;
+            var largeCounter = 0;
+            var fairyCounter = 0;
+            var noDevilCounter = 0;
+            for (int i = 0; i < 100000; i++) {
+                if (Devils.CheckIfDevilAppears(rng, out var id, out var dmg)) {
+                    switch (id) {
+                        case "SmallDevil":
+                            smallCounter++;
+                            Assert.AreEqual(Devils.SMALL_DEVIL_DMG, dmg);
+                            break;
+                        case "MediumDevil":
+                            mediumCounter++;
+                            Assert.AreEqual(Devils.MEDIUM_DEVIL_DMG, dmg);
+                            break;
+                        case "LargeDevil":
+                            largeCounter++;
+                            Assert.AreEqual(Devils.LARGE_DEVIL_DMG, dmg);
+                            break;
+                        case "CharityFairy":
+                            fairyCounter++;
+                            Assert.AreEqual(Devils.FAIRY_HEAL, -dmg);
+                            break;
+                        default:
+                            Assert.Fail($"Unknown id \"{id}\"!");
+                            break;
+                    }
+                } else {
+                    noDevilCounter++;
+                }
+            }
+            var devilCount = (smallCounter + mediumCounter + largeCounter + fairyCounter);
+            var devilChance = devilCount / ((float)(devilCount + noDevilCounter));
+            ComparePercentagesRoughly("Devils", devilChance, Devils.DEVIL_CHANCE);
+            var occTotal = Devils.SMALL_DEVIL_OCC + Devils.MEDIUM_DEVIL_OCC + Devils.LARGE_DEVIL_OCC + Devils.FAIRY_OCC;
+            CompareOccurrance("Small Devil", smallCounter, Devils.SMALL_DEVIL_OCC);
+            CompareOccurrance("Medium Devil", mediumCounter, Devils.MEDIUM_DEVIL_OCC);
+            CompareOccurrance("Large Devil", largeCounter, Devils.LARGE_DEVIL_OCC);
+            CompareOccurrance("Charity Fairy", fairyCounter, Devils.FAIRY_OCC);
+
+            void ComparePercentagesRoughly (string id, float measured, float ideal) {
+                Console.WriteLine($"{id} appeared {(measured * 100):F2}% of the time, ideal value is {(ideal * 100):F2}%");
+                Assert.LessOrEqual(Math.Abs(ideal - measured), 0.01f);  // 1% deviation is okay
+            }
+
+            void CompareOccurrance (string id, int count, int occReference) {
+                var measuredPercentage = count / (float)devilCount;
+                var idealPercentage = occReference / (float)occTotal;
+                ComparePercentagesRoughly(id, measuredPercentage, idealPercentage);
+            }
+        }
+
+    }
+
+}
